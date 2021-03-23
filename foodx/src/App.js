8ff1ae72e257c5ad2ed348647c3b762999ff5c00
Syed Abrar <syedabrar003@gmail.com>
2021-03-23 16:45:59 -0300

    Commit React Boilerplate
    Adding some Boilerplate code

---
+++ foodx/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.figma.com/proto/PreQipZvjzDP8iKMzrVflz/FoodX?node-id=169%3A183&scaling=min-zoom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FoodX
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
